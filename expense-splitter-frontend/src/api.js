a73cc8fc8470253c2523c3e624e0e60eee13f092
Sandra S Prasad <98975384+sandrasprasad@users.noreply.github.com>
2025-12-16 11:06:43 +0530

    Update api.js

--- expense-splitter-frontend/src/api.js
+++ expense-splitter-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8082/api', // Match your Backend Port
+    baseURL: 'http://localhost:8082/api',
 });
 
 // Automatically add Token to headers
